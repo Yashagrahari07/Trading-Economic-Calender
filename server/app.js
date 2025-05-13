2c7124f9616af5d1699a57c16e3cee1eb3358e37
Yash Agrahari <yashagrahari456@gmail.com>
2025-05-13 10:00:30 +0530

    server setup

---
+++ server/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log('MongoDB connected'))
+.catch((err) => console.error(err));
+
+module.exports = app;
